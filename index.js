1af3d1b1a1fbcb7dc381fcee06096a6ae6e084a8
N S <kwb020312@naver.com>
2023-05-31 21:53:27 +0900

    Feat: 중력 가속도 구현

--- index.js
+++ index.js
@@ -4,20 +4,45 @@ const c = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+const gravity = 0.5;
+
 class Player {
   constructor() {
     this.position = {
       x: 100,
       y: 100,
     };
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
     this.width = 100;
     this.height = 100;
   }
 
   draw() {
+    c.fillStyle = "red";
     c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update() {
+    this.draw();
+    this.position.y += this.velocity.y;
+    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+      this.velocity.y += gravity;
+    } else {
+      this.velocity.y = 0;
+    }
   }
 }
 
 const player = new Player();
-player.draw();
+player.update();
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+}
+
+animate();
