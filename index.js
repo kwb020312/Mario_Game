82142307f4696e5c06641c7b2d50d73f160d9fe9
N S <kwb020312@naver.com>
2023-05-31 22:50:42 +0900

    Feat: 우측 이동 구현

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ class Player {
 
   update() {
     this.draw();
+    this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
     if (this.position.y + this.height + this.velocity.y <= canvas.height) {
       this.velocity.y += gravity;
@@ -37,12 +38,23 @@ class Player {
 }
 
 const player = new Player();
-player.update();
+const keys = {
+  right: {
+    pressed: false,
+  },
+  left: {},
+};
 
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
   player.update();
+
+  if (keys.right.pressed) {
+    player.velocity.x = 5;
+  } else {
+    player.velocity.x = 0;
+  }
 }
 
 animate();
@@ -51,6 +63,7 @@ addEventListener("keydown", ({ keyCode }) => {
   switch (keyCode) {
     case 65: {
       console.log("left");
+      keys.left.pressed = true;
       break;
     }
 
@@ -59,11 +72,30 @@ addEventListener("keydown", ({ keyCode }) => {
       break;
 
     case 68:
-      console.log("right");
+      keys.right.pressed = true;
       break;
     case 87:
-      console.log("up");
       player.velocity.y -= 20;
+      break;
+  }
+});
+
+addEventListener("keyup", ({ keyCode }) => {
+  switch (keyCode) {
+    case 65: {
+      console.log("left");
+      keys.left.pressed = false;
+      break;
+    }
+
+    case 83:
+      console.log("down");
+      break;
+
+    case 68:
+      keys.right.pressed = false;
+      break;
+    case 87:
       break;
   }
 });
