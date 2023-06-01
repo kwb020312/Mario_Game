421a2ec5d2b78b8704f30fb8bd90ad8ecea60768
N S <kwb020312@naver.com>
2023-06-01 21:41:38 +0900

    Feat: 우측 추락 감지

--- index.js
+++ index.js
@@ -84,7 +84,8 @@ function animate() {
     player.position.y + player.height <= platform.position.y &&
     player.position.y + player.height + player.velocity.y >=
       platform.position.y &&
-    player.position.x + player.width >= platform.position.x
+    player.position.x + player.width >= platform.position.x &&
+    player.position.x <= platform.position.x + platform.width
   ) {
     player.velocity.y = 0;
   }
